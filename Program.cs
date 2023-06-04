91a6418e94357906d8e558dc7e23ab72306302d4
Proker1337 <artem.sysyuk@mail.ru>
2023-06-04 17:39:46 +0700

    вф
    пав

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace pitsiBi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int da = 0;
+            string da1 = "-7";
+            for (int i = 1007; i > 0 ; i -= 7)
+            {
+                da = i - 7;
+                
+                Console.WriteLine(da + da1);
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
